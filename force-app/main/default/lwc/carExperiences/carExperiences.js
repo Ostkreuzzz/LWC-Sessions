223182378f5426114e838f94d18ef867e6215278
Ostkreuzzz <sulima719@gmail.com>
2023-08-05 18:29:08 +0300

    Finish

--- force-app/main/default/lwc/carExperiences/carExperiences.js
+++ force-app/main/default/lwc/carExperiences/carExperiences.js
@@ -1,51 +1,62 @@
-import { LightningElement, api, track } from "lwc";
-import getExperiences from "@salesforce/apex/CarExperience.getExperiences";
-import { ShowToastEvent } from "lightning/platformShowToastEvent";
-import { NavigationMixin } from "lightning/navigation";
+import { LightningElement, api, track } from 'lwc';
+import getExperiences from '@salesforce/apex/CarExperience.getExperiences';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { NavigationMixin } from 'lightning/navigation';
 
 export default class CarExperiences extends NavigationMixin(LightningElement) {
-  @api carId;
-  @track carExperiences;
 
-  connectedCallback() {
-    this.getCarExperiences();
-  }
+    privateCarId;
+    @track carExperiences = [];
 
-  getCarExperiences() {
-    getExperiences({ carId: this.carId })
-      .then((experiences) => {
-        this.carExperiences = experiences;
-      })
-      .catch((error) => {
-        this.showToast("ERROR", error.body.message, "error");
-      });
-  }
-
-  userClickHandler(event) {
-    const userId = event.target.getAttribute("data-userid");
-    this[NavigationMixin.Navigate]({
-      type: "standard__recordPage",
-      attributes: {
-        recordId: userId,
-        objectApiName: "User",
-        actionName: "view"
-      }
-    });
-  }
-
-  showToast(title, message, variant) {
-    const evt = new ShowToastEvent({
-      title: title,
-      message: message,
-      variant: variant
-    });
-    this.dispatchEvent(evt);
-  }
-
-  get hasExperiences() {
-    if (this.carExperiences) {
-      return true;
+    connectedCallback(){
+        this.getCarExperiences();
     }
-    return false;
-  }
+
+    @api
+    get carId(){
+        return this.privateCarId;
+    }
+
+    set carId(value){
+        this.privateCarId = value;
+        this.getCarExperiences();
+    }
+
+    @api
+    getCarExperiences(){
+        getExperiences({carId : this.privateCarId}).then( (experiences) =>{
+            this.carExperiences = experiences;
+        }).catch((error) =>{
+            this.showToast('ERROR', error.body.message, 'error');
+        })
+    }
+
+    userClickHandler(event){
+        const userId = event.target.getAttribute('data-userid');
+        this[NavigationMixin.Navigate]({
+            type: "standard__recordPage",
+            attributes: {
+              recordId: userId,
+              objectApiName: "User",
+              actionName: "view",
+            }
+          });
+    }
+    
+    showToast(title, message, variant) {
+        const evt = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+        });
+        this.dispatchEvent(evt);
+    }
+
+    get hasExperiences(){
+        if(this.carExperiences.length > 0){
+            return true;
+        }
+        return false;
+    }
+    
 }
